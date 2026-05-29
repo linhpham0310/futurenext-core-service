9c54669c650bdc862635d2cfad50dac17e7d40f8
Linh Pham <linhpham03102004@gmail.com>
2026-05-29 16:37:13 +0700

    feat(be/S2-BE-05): implement updateRole with last admin protection and audit logging

--- src/modules/users/users-admin.controller.ts
+++ src/modules/users/users-admin.controller.ts
@@ -1,10 +1,21 @@
-import { Controller, Get, Query, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Ip,
+  Param,
+  Put,
+  Query,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 import { UserQueryDto } from './dto/user-query.dto';
 import { UserRole } from './entities/user.entity';
 import { Roles } from '@/shared/decorators/roles.decorator';
 import { RolesGuard } from '@/shared/guards/roles.guard';
 import { UsersService } from './services/users.service';
 import { JwtAuthGuard } from '@/shared/guards/jwt-auth.guard';
+import { UpdateRoleDto } from './dto/update-role.dto';
 
 @Controller('admin/users')
 @UseGuards(JwtAuthGuard, RolesGuard)
@@ -15,5 +26,26 @@ export class UsersAdminController {
   @Get()
   async getAllUsers(@Query() query: UserQueryDto) {
     return this.usersService.findAll(query);
+  }
+  // --- [Task: S2-BE-05] PHẦN CODE THÊM MỚI ---
+  @Put(':id/role')
+  async updateUserRole(
+    @Param('id') targetUserId: string,
+    @Body() updateRoleDto: UpdateRoleDto,
+    @Req() req: any, // Request object để lấy thông tin user đang login
+    @Ip() ip: string,
+  ) {
+    const actionById = req.user.id; // Lấy ID của Admin đang thao tác từ JWT Payload (S1)
+
+    await this.usersService.updateRole(
+      targetUserId,
+      updateRoleDto.role,
+      actionById,
+      ip,
+    );
+
+    return {
+      message: 'Cập nhật vai trò người dùng thành công.',
+    };
   }
 }
