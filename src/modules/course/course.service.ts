c6858b9274105bf35a2747e329208c9bbfd46897
linhpham0310 <134529619+linhpham0310@users.noreply.github.com>
2026-06-01 15:06:10 +0700

    feat(S1-CM-03): implement create draft course api (#44)
    * feat(S1-CM-03): implement create draft course api

---
+++ src/modules/course/course.service.ts
@@ -0,0 +1,32 @@
+// src/modules/course/course.service.ts
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../../../prisma/prisma.service';
+import { CreateCourseDto } from './dto/create-course.dto';
+import slugify from 'slugify';
+import { nanoid } from 'nanoid';
+
+@Injectable()
+export class CourseService {
+  constructor(private prisma: PrismaService) {}
+
+  async createDraft(instructorId: string, dto: CreateCourseDto) {
+    const baseSlug = slugify(dto.title, {
+      lower: true,
+      strict: true,
+      locale: 'vi',
+    });
+    const uniqueSlug = `${baseSlug}-${nanoid(6)}`;
+
+    return this.prisma.course.create({
+      data: {
+        title: dto.title,
+        slug: uniqueSlug,
+        description: dto.description,
+        price: dto.price || 0,
+        thumbnailUrl: dto.thumbnailUrl,
+        instructorId: instructorId,
+        status: 'DRAFT',
+      },
+    });
+  }
+}
