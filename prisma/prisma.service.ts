7f311752c2400cdd4dc193494eaf3a98ccf0dcf8
Linh Pham <linhpham03102004@gmail.com>
2026-06-01 14:52:34 +0700

    feat(S1-CM-03): implement create draft course api

--- prisma/prisma.service.ts
+++ prisma/prisma.service.ts
@@ -1,12 +1,8 @@
-// prisma/prisma.service.ts
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService
-  extends PrismaClient
-  implements OnModuleInit, OnModuleDestroy
-{
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
   constructor() {
     super();
   }
