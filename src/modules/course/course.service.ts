dbd1eb1cdc1b822ac6bf3e1eba38505af6386d7b
linhpham0310 <134529619+linhpham0310@users.noreply.github.com>
2026-06-01 15:43:05 +0700

    [S2-CM-01] Implement Add Section API with Auto Order Index (#46)
    * feat(S2-CM-01): implement add section api with auto ordering

--- src/modules/course/course.service.ts
+++ src/modules/course/course.service.ts
@@ -4,6 +4,7 @@ import { PrismaService } from '../../../prisma/prisma.service';
 import { CreateCourseDto } from './dto/create-course.dto';
 import slugify from 'slugify';
 import { nanoid } from 'nanoid';
+import { CreateSectionDto } from './dto/create-section.dto';
 
 @Injectable()
 export class CourseService {
@@ -36,4 +37,25 @@ export class CourseService {
       data: data,
     });
   }
+
+  // TASK S2-CM-01: THÊM CHƯƠNG MỤC MỚI
+  async addSection(courseId: string, dto: CreateSectionDto) {
+    // 1. Tìm order_index lớn nhất hiện tại của khóa học này
+    const lastSection = await this.prisma.section.findFirst({
+      where: { courseId },
+      orderBy: { orderIndex: 'desc' },
+      select: { orderIndex: true },
+    });
+    // 2. Tính toán index mới (Nếu chưa có thì bắt đầu từ 1)
+    const newOrderIndex = lastSection ? lastSection.orderIndex + 1 : 1;
+    // 3. Tạo record mới trong bảng sections
+    return this.prisma.section.create({
+      data: {
+        title: dto.title,
+        courseId: courseId,
+        orderIndex: newOrderIndex,
+      },
+    });
+  }
+
 }
