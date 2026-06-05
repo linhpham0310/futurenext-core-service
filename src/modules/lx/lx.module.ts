59e6de0430f85c1b58742cac949b2fe1aae2c67b
Linh Pham <linhpham03102004@gmail.com>
2026-06-05 15:45:33 +0700

    fix: add PrismaModule and fix LxModule import path

--- src/modules/lx/lx.module.ts
+++ src/modules/lx/lx.module.ts
@@ -1,9 +1,13 @@
+// lx.module.ts
 import { Module } from '@nestjs/common';
 import { LxService } from './lx.service';
 import { LxController } from './lx.controller';
+import { PrismaModule } from 'prisma/prisma.module';
 
 @Module({
-  providers: [LxService],
+  imports: [PrismaModule],
   controllers: [LxController],
+  providers: [LxService],
+  exports: [LxService],
 })
 export class LxModule {}
